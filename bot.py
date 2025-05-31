a205c0087f856cc626e7515044d0b0ea2b144933
Tsugaru Shinuchi <sakata.skyhi@gmail.com>
2025-05-31 10:03:05 +0900

    botファイル修正

--- bot.py
+++ bot.py
@@ -14,35 +14,35 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
+class MyBot(commands.Bot):
+    async def setup_hook():
+        # DB 初期化
+        db_pool=await DB.init_pool()
+        bot.profile_db_pool = db_pool
+        
+        print("✅ DB プールを初期化しました。")
+
+        # Cog を追加
+        initial_extensions = [
+            "cogs.recruitment",
+            "cogs.modals",
+            "cogs.handlers"
+        ]
+        for ext in initial_extensions:
+            await bot.load_extension(ext)
+        print("✅ Cogs を登録しました。")
+
+        # ギルドコマンドを同期
+        guild = discord.Object(id=GUILD_ID)
+        await bot.tree.sync(guild=guild)
+        print("✅ ギルドコマンドを同期しました。")
+
+        from cogs.recruitment import RecruitmentView
+        from cogs.profile import ProfileCog
+        bot.add_view(RecruitmentView())
+        await bot.add_cog(ProfileCog(bot))
+
 bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
-async def setup_hook():
-    # DB 初期化
-    db_pool=await DB.init_pool()
-    bot.profile_db_pool = db_pool
-    
-    print("✅ DB プールを初期化しました。")
-
-    # Cog を追加
-    initial_extensions = [
-        "cogs.recruitment",
-        "cogs.modals",
-        "cogs.handlers"
-    ]
-    for ext in initial_extensions:
-        await bot.load_extension(ext)
-    print("✅ Cogs を登録しました。")
-
-    # ギルドコマンドを同期
-    guild = discord.Object(id=GUILD_ID)
-    await bot.tree.sync(guild=guild)
-    print("✅ ギルドコマンドを同期しました。")
-
-    from cogs.recruitment import RecruitmentView
-    from cogs.profile import ProfileCog
-    bot.add_view(RecruitmentView())
-    await bot.add_cog(ProfileCog(bot))
 
 @bot.event
 async def on_ready():
